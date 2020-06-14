dcebdadff3d6fb71bc30bcd276b00ad4d84a22c7
Jérémie Lumbroso <lumbroso@cs.princeton.edu>
2020-06-14 16:09:33 -0400

    Added `comma.methods` to add load/write/dump wrapper methods

--- comma/__init__.py
+++ comma/__init__.py
@@ -5,3 +5,6 @@ Library to make CSV reading/writing fun and enjoyable!
 
 __version__ = "0.1.0"
 __author__ = "Jérémie Lumbroso <lumbroso@cs.princeton.edu>"
+
+
+from comma.methods import load
---
+++ comma/methods.py
@@ -0,0 +1,62 @@
+
+import typing
+
+import comma.classes.file
+import comma.classes.row
+import comma.classes.table
+import comma.helpers
+import comma.typing
+
+
+__author__ = "Jérémie Lumbroso <lumbroso@cs.princeton.edu>"
+
+__all__ = [
+    "load"
+]
+
+
+def load(
+    source: comma.typing.SourceType,
+    encoding: str = None,
+    delimiters: typing.Optional[typing.Iterable[str]] = None,
+):
+    """
+
+    """
+
+    # Use the helper method to open the data, parse it and return
+    # a CommaInfo typed dictionary.
+
+    csv_comma_info = comma.helpers.open_csv(
+        source=source,
+        encoding=encoding,
+        delimiters=delimiters,
+    )
+
+    if csv_comma_info is None:
+        return
+
+    csv_rows_raw = csv_comma_info["rows"]
+    csv_header = csv_comma_info["header"]
+
+    # create CommaFile object
+    parent_comma_file = comma.classes.file.CommaFile(
+        header=csv_header,
+    )
+
+    def _make_comma_row(csv_row_data):
+        return comma.classes.row.CommaRow(
+            csv_row_data,
+            parent=parent_comma_file,
+        )
+
+    csv_comma_rows = list(map(_make_comma_row, csv_rows_raw))
+
+    csv_comma_table = comma.classes.table.CommaTable(
+        csv_comma_rows,
+        parent=parent_comma_file
+    )
+
+    return csv_comma_table
+
+
