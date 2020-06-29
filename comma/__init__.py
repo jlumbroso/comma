5e02d2d906499785ad5f2a21c15f87c62b6b3192
Jérémie Lumbroso <lumbroso@cs.princeton.edu>
2020-06-29 00:22:37 -0400

    released v0.1.4 to PyPi with alpha version warning

--- comma/__init__.py
+++ comma/__init__.py
@@ -3,9 +3,16 @@
 Library to make CSV reading/writing fun and enjoyable!
 """
 
-__version__ = "0.1.3"
+__version__ = "0.1.4"
 __author__ = "Jérémie Lumbroso <lumbroso@cs.princeton.edu>"
 
 
 from comma.methods import load
 from comma.methods import dump, dumps
+
+import warnings
+warnings.warn(
+    "Thank you for trying comma. This package is currently in "
+    "development (alpha release). If you find any issues, please "
+    "report. A final version will be released by mid-July."
+)
--- tests/test_comma.py
+++ tests/test_comma.py
@@ -1,5 +1,42 @@
-from comma import __version__
+import os
+import re
+
+import comma
 
 
-def test_version():
-    assert __version__ == "0.1.3"
+VERSION_NUMBER = "0.1.4"
+POETRY_FILE = "pyproject.toml"
+
+
+def get_pyproject_version() -> str:
+    """
+    Returns the version number contained in the Poetry package
+    definition file.
+    """
+    comma_init_filepath = comma.__file__
+    comma_root_dir = os.path.abspath(
+        os.path.join(os.path.dirname(comma.__file__), ".."))
+
+    pyproject_path = os.path.join(comma_root_dir, POETRY_FILE)
+
+    assert os.path.exists(pyproject_path), "check poetry file exists"
+
+    pyproject_src = open(pyproject_path).read()
+    version_match = re.search(
+        r"version\s*=\s*\"([^\"]*)\"",
+        pyproject_src)
+
+    assert version_match, "check poetry file version info can be parsed"
+    return version_match.group(1)
+
+
+def test_package_version():
+    assert comma.__version__ == VERSION_NUMBER, "check version in comma/__init__.py"
+
+
+def test_pyproject_version():
+    assert get_pyproject_version() == VERSION_NUMBER, "check version in pyproject.toml"
+
+
+def test_consistent_version():
+    assert comma.__version__ == get_pyproject_version(), "check version consistency"
