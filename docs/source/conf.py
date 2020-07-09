06e3664c463a5a3498004ba5289f4e410103f241
Jérémie Lumbroso <lumbroso@cs.princeton.edu>
2020-07-08 23:19:09 -0400

    Minor rename of docs folders

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -51,7 +51,7 @@ extensions = [
 ]
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ["templates"]
+templates_path = ["_templates"]
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
@@ -74,4 +74,4 @@ html_theme_options = {
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ["static"]
+html_static_path = ["_static"]
