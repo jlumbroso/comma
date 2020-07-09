a961848644fc2d1b6e845b2b340229a6aa99f261
Jérémie Lumbroso <lumbroso@cs.princeton.edu>
2020-07-08 23:16:42 -0400

    Initial seedling of the Sphinx documentation

--- comma/__init__.py
+++ comma/__init__.py
@@ -6,6 +6,7 @@ Library to make CSV reading/writing fun and enjoyable!
 __version__ = "0.1.4"
 __author__ = "Jérémie Lumbroso <lumbroso@cs.princeton.edu>"
 
+version_info = tuple(int(v) if v.isdigit() else v for v in __version__.split('.'))
 
 from comma.methods import load
 from comma.methods import dump, dumps
